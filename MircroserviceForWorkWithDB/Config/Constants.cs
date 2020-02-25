81d84da062f04138a94c97260eb5a8ef3fb48e15
MaksymYaroshenko <53305575+MaksymYaroshenko@users.noreply.github.com>
2020-02-25 18:48:27 +0200

    Add Weather API key

---
+++ MircroserviceForWorkWithDB/Config/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace MircroserviceForWorkWithDB.Config
+{
+    public static class Constants
+    {
+        public const string OPEN_WEATHER_APPID = "ee65b4dc730c16100f228531b630b496";
+    }
+}
