dc8e4ebb2bcaf80c28adb000fa8059a732107594
MaksymYaroshenko <53305575+MaksymYaroshenko@users.noreply.github.com>
2020-02-28 11:29:31 +0200

    Add Unit tests

---
+++ MicroserviceForWorkWithDB.Tests/WeatherControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using MircroserviceForWorkWithDB.Controllers;
+using MircroserviceForWorkWithDB.Database.Entities;
+using Xunit;
+
+namespace MicroserviceForWorkWithDB.Tests
+{
+    public class WeatherControllerTests
+    {
+        readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                .AddConsole()
+                .AddEventLog();
+        });
+
+        [Fact]
+        public void GetNotNull()
+        {
+            WeatherController controller = new WeatherController(_loggerFactory);
+            City city = controller.Get("Sumy") as City;
+            Assert.NotNull(city);
+        }
+
+        [Fact]
+        public void GetNull()
+        {
+            WeatherController controller = new WeatherController(_loggerFactory);
+            City city = controller.Get(string.Empty) as City;
+            Assert.Null(city);
+        }
+
+    }
+}
